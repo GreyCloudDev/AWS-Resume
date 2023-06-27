f2a2fa484aed7e7f72c87fae47e4212a93fcbc9c
GreyWoulf <greywoulf88@outlook.com>
2023-06-27 14:12:50 -0400

    added eventListener and code for visit count

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,19 @@
-const functionAPI
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionAPI = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).this(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
